8b14bd6aed8e094d40c53a1ca937d979437c439b
Aotokitsuruya <contact@aotoki.me>
2025-01-05 22:33:38 +0800

    chore: allow print memory records

--- lib/autoflux/memory.rb
+++ lib/autoflux/memory.rb
@@ -3,11 +3,12 @@
 module Autoflux
   # The Memory is a class to store the memory of the workflow.
   class Memory
-    attr_reader :data
+    attr_reader :data, :verbose
 
     # @rbs data: Array[Hash]
-    def initialize(data: [])
+    def initialize(data: [], verbose: false)
       @data = data
+      @verbose = verbose
       freeze
     end
 
@@ -15,6 +16,7 @@ module Autoflux
     #
     # @rbs data: Hash
     def push(data)
+      puts JSON.pretty_generate(data) if verbose
       @data.push(data)
     end
 
